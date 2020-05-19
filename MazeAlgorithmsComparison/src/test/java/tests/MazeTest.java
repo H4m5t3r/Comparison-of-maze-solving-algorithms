8c20d8605090a360f2665ca4204d77d8cc5b6d9c
TheHamster <anton.taleiko@gmail.com>
2020-05-19 16:56:21 +0300

    Added two tests for the Maze class.

---
+++ MazeAlgorithmsComparison/src/test/java/tests/MazeTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import maze.domain.Maze;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author taleiko
+ */
+public class MazeTest {
+    private Maze maze;
+    
+    @Before
+    public void setUp() {
+        this.maze = new Maze(10, 10);
+    }
+    
+    @Test
+    public void mazeBaseIsOnlyMadeOfWalls() {
+        for (int y = 0; y < maze.getMaze().length; y++) {
+            for (int x = 0; x < maze.getMaze()[0].length; x++) {
+                assertTrue(maze.getMaze()[y][x] == '#');
+            }
+        }
+    }
+    
+    @Test
+    public void removeWallWorks() {
+        for (int y = 0; y < maze.getMaze().length; y++) {
+            for (int x = 0; x < maze.getMaze()[0].length; x++) {
+                assertTrue(maze.getMaze()[y][x] == '#');
+                maze.removeWall(x, y);
+                assertTrue(maze.getMaze()[y][x] == ' ');
+            }
+        }
+    }
+}
